e7666f791ecbd0c3e114ef78610aa7bbac9ea18a
yoeranmoreel <yoeranmoreel@gmail.com>
2026-06-18 15:00:45 +0200

    Update sw.js
    PWA install

--- sw.js
+++ sw.js
@@ -1,18 +1,44 @@
-const CACHE_NAME = 'ambulante-todo-v1';
+const CACHE_NAME = 'ambulante-todo-v2';
 
-// Cache alleen de HTML pagina zelf
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-128.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(['./index.html']);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-// Probeer altijd network eerst, val terug op cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, copy));
+
+        return response;
+      })
       .catch(() => caches.match(event.request))
   );
 });
